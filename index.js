4dad910bc55b3b417e64441da685596a38e0de20
Michael Vanasse <michael.vanasse@optiv.com>
2017-11-28 11:45:25 -0600

    allow flag for ignoring files

--- index.js
+++ index.js
@@ -39,60 +39,89 @@ function confrc(cwd, env, opts) {
     cwd = opts.cwd || process.cwd();
   }
 
-  if (!opts.hasOwnProperty('cwd') && !opts.hasOwnProperty('env') && !opts.hasOwnProperty('dirs')) {
+  if (!hasOpts(['cwd', 'env', 'dirs', 'overrides'])) {
     opts = {env: opts};
   }
 
-  env = Object.assign({}, process.env, opts.env || {});
+  if (!opts.env) {
+    opts.env = {};
+  }
 
-  const dirs = [
+  if (!opts.overrides) {
+    opts.overrides = {};
+  }
+
+  env = Object.assign({}, process.env, opts.env, opts.overrides);
+
+  // remove non-environment variable props
+  delete env['_'];
+
+  let dirs = [];
+
+  dirs.push(
     'etc',
     'config',
-    '.config',
-  ].concat(opts.dirs || [])
-   .map(dir => join(cwd, dir))
-   .filter(dir => exists(dir))
-   .map(dir => dir.replace(cwd, ''));
+    '.config'
+  );
+
+  dirs = dirs
+    .concat(opts.dirs || [])
+    .map(dir => join(cwd, dir))
+    .filter(dir => exists(dir))
+    .map(dir => dir.replace(cwd, ''));
 
   // search for the files, read if they exist, and merge the output
   let values = [].concat(
     search('common'),
     search('default'),
     search('local'),
-    `.env`
-  ).reduce((prev, next) => prev.concat(next), [])
-   .reduce(read, env)
+  )
+
+  if (!env['IGNORE_ENV']) {
+    values.push('.env');
+  }
+
+  values = values
+    .reduce((prev, next) => prev.concat(next), [])
+    .reduce(read, {});
+
+  Object.assign(values, opts.overrides);
 
   // now look for environment-specific config files, since it may
   // have been changed in an env file above
-  values = search(values.NODE_ENV).reduce(read, values);
-
-  // remove non-environment variable props
-  delete values['_'];
+  values = search(values.NODE_ENV || env.NODE_ENV).reduce(read, values);
 
   function config() {}
 
   Object.defineProperties(config, {
-    env: {value: values},
+    merged: {value: Object.assign({}, env, values)},
+    values: {value: values},
+    env: {value: env},
     cwd: {value: cwd},
   })
 
   const proxy = new Proxy(config, {
     get: function(target, name) {
       if (target.hasOwnProperty(name)) return target[name];
-      if (target.env.hasOwnProperty(name)) return target.env[name];
+      if (target.merged.hasOwnProperty(name)) return target.merged[name];
     },
     apply: function(target, thisArg, args) {
       const names = [].concat(args.shift());
       const fallback = args.pop();
-      return get.call(target.env, names, fallback);
+      return get.call(target.merged, names, fallback);
     }
   });
 
   return Object.freeze(proxy);
 
+  function hasOpts(list) {
+    return opts.hasOwnProperty(list.shift()) || (list.length && hasOpts(list));
+  }
+
   function search(name) {
     if (!name) return [];
+    if (env['IGNORE_ENVS']) return [];
+    if (env[`IGNORE_ENV_${name.toUpperCase()}`]) return [];
 
     const paths = [`.env.${name}`];
 
