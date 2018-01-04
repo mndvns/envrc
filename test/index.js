7cc35a4fe15d448b23c18d59b662d3e1330f3492
Michael Vanasse <michael.vanasse@optiv.com>
2018-01-03 20:02:27 -0600

    store each library call

--- test/index.js
+++ test/index.js
@@ -26,7 +26,7 @@ describe('ignores', () => {
   });
 
   it('should ignore files that match globs from evironment', () => {
-    const conf = lib(fixtures, {'ENVRC_IGNORE': 'etc/*'});
+    const conf = lib(fixtures, {ENVRC_IGNORE: 'etc/*'});
     conf.value.should.equal('something');
     conf.other_value.should.equal('original');
   });
@@ -34,12 +34,12 @@ describe('ignores', () => {
 
 describe('env', () => {
   it('should accept variables explicitly or implicitly', () => {
-    lib({stuff: 'good'}).stuff.should.equal('good')
-    lib({env: {stuff: 'good'}}).stuff.should.equal('good')
+    lib({stuff: 'good'}).stuff.should.equal('good');
+    lib({env: {stuff: 'good'}}).stuff.should.equal('good');
   });
 
   it('should read environment variables', () => {
-    process.env.FOOBAR = 'hi'
+    process.env.FOOBAR = 'hi';
     lib().FOOBAR.should.equal('hi');
   });
 
@@ -48,20 +48,40 @@ describe('env', () => {
     lib({env: {NODE_ENV: 'development'}}).NODE_ENV.should.equal('development');
   });
 
-  it('should fallback to non-environment properties', () => {
-    lib().env.should.be.a.Object();
-    lib({env: {FOOBAR: 'hi'}}).env.FOOBAR.should.equal('hi');
-    lib().cwd.should.be.a.String();
-    lib().cwd.should.equal(process.cwd());
+  describe('fallbacks', () => {
+    it('should fallback to non-environment properties', () => {
+      lib().env.should.be.a.Object();
+      lib({env: {FOOBAR: 'hi'}}).env.FOOBAR.should.equal('hi');
+      lib().cwd.should.be.a.String();
+      lib().cwd.should.equal(process.cwd());
+    });
+
+    it('should fallback to arguments when called', () => {
+      should.not.exist(lib()('BAZ'));
+      lib({env: {BAZ: 'cool'}})('BAZ').should.equal('cool');
+      lib({env: {ZUN: 'keep'}})('ZUN', 'other').should.equal('keep');
+      lib({env: {BUZ: 'cool'}})(['BAZ', 'BUZ']).should.equal('cool');
+    });
+
+    it('should use fallback as value if arguments are exhausted', () => {
+      lib({env: {}})('BAZ', 'cool').should.equal('cool');
+      lib()(['BAZ', 'BUZ', 'QUX'], 'oh well').should.equal('oh well');
+    });
   });
 
-  it('should fallback to arguments when called', () => {
-    should.not.exist(lib()('BAZ'));
-    lib({env: {BAZ: 'cool'}})('BAZ').should.equal('cool');
-    lib({env: {BUZ: 'cool'}})(['BAZ', 'BUZ']).should.equal('cool');
-  });
+  describe('variables', () => {
+    it('should count the stored variables', () => {
+      lib.count.should.be.a.Number();
+      lib.count.should.be.greaterThan(10);
+    });
 
-  it('should use fallback as value if arguments are exhausted', () => {
-    lib()(['BAZ', 'BUZ', 'QUX'], 'oh well').should.equal('oh well');
+    it('should store this variables', () => {
+      let keys = Object.keys(lib.variables);
+      let values = Object.values(lib.variables);
+      keys.length.should.be.a.Number();
+      keys.length.should.be.greaterThan(5);
+      let count = values.reduce((prev, v) => (prev += v.length), 0);
+      count.should.equal(lib.count);
+    });
   });
 });
